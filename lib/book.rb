9f362c42cca042bd6a8dbb40b73b6c1d2ac33666
Lj <tnedutsl457892@gmail.com>
2021-06-28 01:36:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
+  GENRES = []
 
+  def initialize(title)
+    @title = title
+ def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+attr_accessor :color, :size, :material, :condition
+ attr_reader :brand
+
+ BRANDS = []
+ def initialize(brand)
+   @brand = brand
+   BRANDS << @brand
+   BRANDS.uniq!
+ end
+
+ def cobble
